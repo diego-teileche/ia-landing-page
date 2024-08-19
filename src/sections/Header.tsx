0b6460890d7da71c51189d1a63204bd2b1693024
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-19 20:02:32 -0300

    Hero Section | Styles added

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,9 +4,9 @@ import Button from "@/components/Button"
 
 export const Header = () => {
   return (
-    <header className="border-b border-white/15 py-4 md:border-none">
+    <header className="sticky top-0 z-10 border-b border-white/15 py-4 md:border-none">
       <div className="container">
-        <div className="mx-auto flex max-w-2xl items-center justify-between rounded-xl border-white/15 md:border md:p-2.5">
+        <div className="mx-auto flex max-w-2xl items-center justify-between rounded-xl border-white/15 backdrop-blur md:border md:p-2.5">
           <div>
             <div className="inline-flex h-10 w-10 items-center justify-center rounded-lg border border-white/15">
               <LogoIcon className="h-8 w-8" />
