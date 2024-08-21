cbfa33a5ffbca7d3b86eed778dff87a8a479c402
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-21 19:59:40 -0300

    Testimonials Section | Starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Features } from "@/sections/Features"
 import { Header } from "@/sections/Header"
 import { Hero } from "@/sections/Hero"
 import { LogoTicker } from "@/sections/LogoTicker"
+import { Testimonials } from "@/sections/Testimonials"
 
 export default function Home() {
   useTitleChanger({
@@ -17,6 +18,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <Features />
+      <Testimonials />
     </>
   )
 }
