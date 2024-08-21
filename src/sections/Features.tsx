d8fa2175edb0a972a0d64c2114123ff734806f45
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-21 19:50:24 -0300

    FeaturesPage | Styles and image added

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { DotLottiePlayer } from "@dotlottie/react-player"
+import productImage from "@/assets/product-image.png"
 
 const tabs = [
   {
@@ -30,23 +31,43 @@ const tabs = [
 
 export const Features = () => {
   return (
-    <section>
+    <section className="py-20 md:py-24">
       <div className="container">
-        <h2>Elevate your SEO efforts</h2>
-        <p>
+        <h2 className="text-center text-5xl font-medium tracking-tighter md:text-6xl">
+          Elevate your SEO efforts
+        </h2>
+        <p className="mx-auto mt-5 max-w-2xl text-center text-lg tracking-tight text-white/70 md:text-xl">
           From small startups to large enterprises, our IA-driven tool has
           revolutionized the way businesses approach SEO
         </p>
 
-        {tabs.map((tab) => (
-          <div key={tab.title}>
-            <DotLottiePlayer src={tab.icon} className="size-5" autoplay />
+        <div className="mt-10 flex flex-col gap-3 lg:flex-row">
+          {tabs.map((tab) => (
+            <div
+              key={tab.title}
+              className="flex items-center gap-2.5 rounded-xl border border-white/15 p-2.5 lg:flex-1"
+            >
+              <div className="inline-flex size-12 items-center justify-center rounded-lg border border-white/15">
+                <DotLottiePlayer src={tab.icon} className="size-5" autoplay />
+              </div>
 
-            <div>{tab.title}</div>
+              <div className="font-medium">{tab.title}</div>
 
-            {tab.isNew && <div>new</div>}
-          </div>
-        ))}
+              {tab.isNew && (
+                <div className="rounded-full bg-[#8c44ff] px-2 py-0.5 text-xs font-semibold text-black">
+                  new
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-3 rounded-xl border border-white/20 p-2.5">
+          <div
+            className="aspect-video rounded-lg border border-white/20 bg-cover"
+            style={{ backgroundImage: `url(${productImage.src})` }}
+          ></div>
+        </div>
       </div>
     </section>
   )
