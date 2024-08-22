2b1afd9176c6ac05440d855add0b0211358d9a27
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-22 09:16:31 -0300

    Call to Action Section | Styles
    - Title styles added
    - Paragraph styles added
    - Button styles added
    - Some section styles added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -2,11 +2,19 @@ import Button from "@/components/Button"
 
 export const CallToAction = () => {
   return (
-    <section>
-      <div>
-        <h2>AI-driven SEO for everyone</h2>
-        <p>Achive clear, impactful results without the complexity</p>
-        <Button>Join waitlist</Button>
+    <section className="py-20">
+      <div className="container">
+        <div className="border border-white/15 py-24">
+          <h2 className="text-center text-5xl font-medium tracking-tighter">
+            AI-driven SEO for everyone
+          </h2>
+          <p className="mt-5 px-4 text-center text-lg tracking-tight text-white/70">
+            Achive clear, impactful results without the complexity
+          </p>
+          <div className="mt-8 flex justify-center">
+            <Button>Join waitlist</Button>
+          </div>
+        </div>
       </div>
     </section>
   )
