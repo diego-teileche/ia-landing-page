fde39d33cc70cc8b361360502ce258f3e535bb1f
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-22 10:16:15 -0300

    Footer | Responsive design styles added

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -7,44 +7,44 @@ export const Footer = () => {
   return (
     <footer className="border-t border-white/15 py-5">
       <div className="container">
-        <div className="flex flex-col gap-8">
-          <div className="flex items-center gap-2">
+        <div className="flex flex-col gap-8 lg:flex-row lg:items-center">
+          <div className="flex items-center gap-2 lg:flex-1">
             <Logo className="size-6" />
             <div className="font-medium">AI Startup Landing Page</div>
           </div>
-          <nav className="flex flex-col gap-5">
+          <nav className="flex flex-col gap-5 lg:flex-1 lg:flex-row lg:justify-center lg:gap-7">
             <a
               href="#"
-              className="text-xs text-white/70 transition hover:text-white"
+              className="text-xs text-white/70 transition hover:text-white md:text-sm"
             >
               Features
             </a>
             <a
               href="#"
-              className="text-xs text-white/70 transition hover:text-white"
+              className="text-xs text-white/70 transition hover:text-white md:text-sm"
             >
               Developers
             </a>
             <a
               href="#"
-              className="text-xs text-white/70 transition hover:text-white"
+              className="text-xs text-white/70 transition hover:text-white md:text-sm"
             >
               Company
             </a>
             <a
               href="#"
-              className="text-xs text-white/70 transition hover:text-white"
+              className="text-xs text-white/70 transition hover:text-white md:text-sm"
             >
               Blog
             </a>
             <a
               href="#"
-              className="text-xs text-white/70 transition hover:text-white"
+              className="text-xs text-white/70 transition hover:text-white md:text-sm"
             >
               Changelog
             </a>
           </nav>
-          <div className="flex gap-5">
+          <div className="flex gap-5 lg:flex-1 lg:justify-end">
             <XSocial className="text-white/40 transition hover:cursor-pointer hover:text-white" />
             <InstaSocial className="text-white/40 transition hover:cursor-pointer hover:text-white" />
             <YTSocial className="text-white/40 transition hover:cursor-pointer hover:text-white" />
