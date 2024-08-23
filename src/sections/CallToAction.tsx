37aaf53aef377daea3f6adf00083ccb9d9818fec
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-22 21:44:42 -0300

    Call to Actions Section | Radial gradient on mouse move animation added

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,11 +1,18 @@
 import Button from "@/components/Button"
 import starsBg from "@/assets/stars.png"
 import gridLines from "@/assets/grid-lines.png"
-import { motion, useScroll, useTransform } from "framer-motion"
+import {
+  motion,
+  useMotionTemplate,
+  useScroll,
+  useTransform,
+} from "framer-motion"
 import { useRef } from "react"
+import { useRelativeMousePosition } from "@/hooks/useRelativeMousePosition"
 
 export const CallToAction = () => {
   const sectionRef = useRef<HTMLElement>(null)
+  const borderedDivRef = useRef<HTMLDivElement>(null)
 
   const { scrollYProgress } = useScroll({
     target: sectionRef,
@@ -14,26 +21,36 @@ export const CallToAction = () => {
 
   const backgroundPositionY = useTransform(scrollYProgress, [0, 1], [-300, 300])
 
+  const [mouseX, mouseY] = useRelativeMousePosition(borderedDivRef)
+
+  const maskImage = useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px, black, transparent)`
+
   return (
     <section ref={sectionRef} className="py-20 md:py-24">
       <div className="container">
         <motion.div
+          ref={borderedDivRef}
           animate={{ backgroundPositionX: starsBg.width }}
           transition={{
             repeat: Infinity,
             duration: 60,
             ease: "linear",
           }}
-          className="relative overflow-hidden rounded-xl border border-white/15 py-24"
+          className="group relative overflow-hidden rounded-xl border border-white/15 py-24"
           style={{
             backgroundPositionY,
             backgroundImage: `url(${starsBg.src})`,
           }}
         >
           <div
-            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay transition duration-700 [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0"
             style={{ backgroundImage: `url(${gridLines.src})` }}
           ></div>
+          <motion.div
+            className="absolute inset-0 bg-[rgb(74,32,138)] opacity-0 bg-blend-overlay transition duration-700 group-hover:opacity-100"
+            style={{ maskImage, backgroundImage: `url(${gridLines.src})` }}
+          ></motion.div>
+
           <div className="relative">
             <h2 className="mx-auto max-w-sm text-center text-5xl font-medium tracking-tighter md:text-6xl">
               AI-driven SEO for everyone
